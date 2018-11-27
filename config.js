7d7deffb7915ab764a99fcdc98ef5e08512685de
FHGDev <gideon.lingle1@gmail.com>
2018-11-26 23:38:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,11 @@ module.exports = {
   songs: [
   {name: "Better Now", artist: "Post Malone", url: "https://www.youtube.com/watch?v=UYwF-jdcVjY"},
   {name: "God's Plan", artist: "Drake", url: "https://www.youtube.com/watch?v=xpVfcZ0ZcFM"},
-  {name: "Girls Like You", artist: "Maroon 5", url: "https://www.youtube.com/watch?v=aJOTlE1K90k"}
+  {name: "Girls Like You", artist: "Maroon 5", url: "https://www.youtube.com/watch?v=aJOTlE1K90k"},
+  {name: "Animals", artist: "Maroon 5", url: "https://www.youtube.com/watch?v=qpgTC9MDx1o"},
+  {name: "Nobody Can Save Me", artist: "Linkin Park", url: "https://www.youtube.com/watch?v=FY9v147BZuE"},
+  {name: "Heavy [explicit]", artist: "Linkin Park", url: "https://www.youtube.com/watch?v=5dmQ3QWpy1Q"},
+    {name: "Happier", artist: "marshmello", url: "https://www.youtube.com/watch?v=RE87rQkXdNw"},
+    {name: "High Hopes", artist: "Panic! At the Disco", url: "https://www.youtube.com/watch?v=IPXIgEAGe4U"} 
   ]
 }
