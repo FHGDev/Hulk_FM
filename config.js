db6cde28ff2d38d1413bd462f8e9d70471ceb552
FHGDev <gideon.lingle1@gmail.com>
2018-11-26 21:47:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   songs = [
-  {name: "Better Now", artist: "Post Malone", url: "},
-  {},
-  {}
+  {name: "Better Now", artist: "Post Malone", url: "https://www.youtube.com/watch?v=UYwF-jdcVjY"},
+  {name: "God's Plan", artist: "Drake", url: "https://www.youtube.com/watch?v=xpVfcZ0ZcFM"},
+  {name: "Girls Like You", artist: "Maroon 5", url: "https://www.youtube.com/watch?v=aJOTlE1K90k"}
   ]
 }
